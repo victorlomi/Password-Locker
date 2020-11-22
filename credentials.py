30e4fc1534a0fc0179f186d777d9ba3951f1c2bb
plutos28 <gamevictor28@gmail.com>
2020-11-22 21:22:59 +0300

    make tests for credentials class pass

--- credentials.py
+++ credentials.py
@@ -4,13 +4,13 @@ class Credentials():
     """Keeps track of the user's accounts and their credentials."""
 
     def __init__(self):
-        """Store the accounts they user as added(Twitter by default)."""
+        """Store the accounts the user has added(Twitter by default)."""
         self.accounts = {"twitter": Account()};
 
-    def add_account(self, account):
+    def add_account(self, name, account):
         """Add an account and its credentials to the accounts."""
-        pass
+        self.accounts[name] = account
 
-    def remove_account(self, account):
+    def remove_account(self, name):
         """Remove an account and its credentials to the accounts."""
-        pass
+        del self.accounts[name]
--- test_credentials.py
+++ test_credentials.py
@@ -1,5 +1,7 @@
 import unittest
+
 from credentials import Credentials
+from account import Account
 
 class TestCredentials(unittest.TestCase):
     """Tests for the class Credentials."""
@@ -7,16 +9,16 @@ class TestCredentials(unittest.TestCase):
     def test_add_account(self):
         """Test that an account can be added."""
         credentials = Credentials()
-        credentials.add_account({"facebook": {}})
+        credentials.add_account("facebook", Account())
 
-        self.assertTrue("facebook" in credentials)
+        self.assertTrue("facebook" in credentials.accounts)
     
     def test_remove_account(self):
         """Test that an account can be removed"""
         credentials = Credentials()
         credentials.remove_account("twitter")
 
-        self.assertFalse("twitter" in credentials)
+        self.assertFalse("twitter" in credentials.accounts)
 
 
 unittest.main()
