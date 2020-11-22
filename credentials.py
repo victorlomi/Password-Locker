6649cdab01b36358fc9fd60bc5c83c53ad8d7a83
plutos28 <gamevictor28@gmail.com>
2020-11-22 20:58:32 +0300

    add credientials class with unimplemented methods

---
+++ credentials.py
@@ -0,0 +1,14 @@
+class Credentials():
+    """Keeps track of the user's accounts and their credentials."""
+
+    def __init__(self):
+        """Store the accounts they user as added(Twitter by default)."""
+        self.accounts = {"twitter": {}};
+
+    def add_account(self, account):
+        """Add an account and its credentials to the accounts"""
+        pass
+
+    def remove_account(self, account):
+        """Remove an account and its credentials to the accounts"""
+        pass
