b9f03962f22a96b117f35174437fdd4e6e5fbc92
plutos28 <gamevictor28@gmail.com>
2020-11-22 20:31:30 +0300

    add function that shows the title card

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ def show_main_menu():
     print("  2.Add Account")
     print("  3.Remove Account")
 
+def show_title_card():
+    print("****************************************************")
+    print("********** Welcome to Password Locker!  ************")
+    print("* The best and simplest password manager available *")
+    print("****************************************************\n")
+
+show_title_card()
 show_main_menu()
