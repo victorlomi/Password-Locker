0ad6d4d6357ebd08b04b2c10485dfeae6bf3455a
plutos28 <gamevictor28@gmail.com>
2020-11-22 21:31:16 +0300

    add comments to make dividing the function easier

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+from credentials import Credentials
+
+credentials = Credentials()
+
 def show_main_menu():
+    # show the main menu banner
     print("\n****************************************************")
     print("******************* Main Menu  *********************")
     print("****************************************************")
 
+    # print out the available accounts
     print("Accounts:")
-    print("  1.Twitter")
+    i = 0
+    for account in credentials.accounts:
+        i += 1
+        print(f"  {i}.{account.title()}")
+
+    # print out the available actions
     print("Actions:")
-    print("  2.Add Account")
-    print("  3.Remove Account")
+    print("  a.Add Account")
+    print("  b.Remove Account")
 
 def show_title_card():
     print("****************************************************")
